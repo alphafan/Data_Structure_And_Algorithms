51b2bafdf60ff2bf73a6a256a41d18c91f6eda1b
Yufan Zheng <yufan.zheng1@gmail.com>
2018-03-26 20:06:47 +0800

    get value

--- list/list_impl.py
+++ list/list_impl.py
@@ -84,7 +84,7 @@ class List(object):
     def delete(self, value):
         """ Delete the first node with value """
         if self.head is None:
-            return self
+            raise Exception('Empty List')
         if self.head.value == value:
             self.head = self.head.next
             return self
@@ -101,7 +101,7 @@ class List(object):
     def deleteAll(self, value):
         """ Delete all nodes with the value in list """
         if self.head is None:
-            return self
+            raise Exception('Empty List')
         while self.head.value == value:
             self.head = self.head.next
             if self.head is None:
@@ -126,7 +126,7 @@ class List(object):
             self.head = self.head.next
             return self
         i, node = 0, self.head
-        while i < k-1:
+        while i < k:
             node = node.next
             i += 1
             if node.next is None:
@@ -135,6 +135,58 @@ class List(object):
             raise Exception('Position k: ({}) out of range'.format(k))
         node.next = node.next.next
         return self
+
+    # ---------------------------------------------
+    # Get value
+    # ---------------------------------------------
+
+    def get(self, k):
+        """ Get the node value at k position """
+        if self.head is None and k != 0:
+            raise Exception('Empty List')
+        if k < 0:
+            raise Exception('Position k: ({}) can not be negative'.format(k))
+        if k == 0:
+            return self.head
+        i, node = 0, self.head
+        while i < k:
+            node = node.next
+            i += 1
+            if node is None:
+                raise Exception('k: ({}) out of range'.format(k))
+        return node.value
+
+    def getKthLast(self, k):
+        """ Return the last Kth value. """
+        if k < 0:
+            raise Exception('Position k: ({}) can not be negative'.format(k))
+        if self.head is None and k != 0:
+            raise Exception('k out of range')
+        if self.head is None and k == 0:
+            return None
+        i, fast, slow = 0, self.head, self.head
+        while i < k:
+            fast = fast.next
+            i += 1
+            if fast is None and i != k:
+                raise Exception('Position k: ({}) out of range'.format(k))
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        return slow
+
+    def getMidItem(self):
+        if self.head is None or self.head.next is None:
+            return None
+        slow, fast = self.head, self.head
+        while slow and fast:
+            if fast.next is None:
+                break
+            fast = fast.next.next
+            if fast is None:
+                break
+            slow = slow.next
+        return slow
 
     # ---------------------------------------------
     # Reverse list element(s)
@@ -152,3 +204,11 @@ class List(object):
             cur = nxt
         self.head = pre
         return self
+
+
+lst = List()
+lst.insert(0)
+lst.insert(1)
+lst.insert(2)
+lst.insert(3)
+print(lst.getMidItem())
