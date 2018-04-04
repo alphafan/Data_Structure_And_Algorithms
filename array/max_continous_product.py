5b3ace88ab926cac052180ae9ad1a94a0f3fa2c1
Yufan Zheng <yufan.zheng1@gmail.com>
2018-04-04 12:05:41 +0800

    max product

--- array/max_continous_product.py
+++ array/max_continous_product.py
@@ -1,15 +1,19 @@
-import random
 
 
-def maxContinuousProduct(nums):
-    maxEndingHere = minEndingHere = maximum = nums[0]
+def maxProductSubArray(nums):
+
+    maxSoFar = 0
+    maxEndingHere = nums[0]
+    minEndingHere = nums[0]
+
     for num in nums[1:]:
         maxEndingHere = max(maxEndingHere*num, minEndingHere*num, num)
-        minEndingHere = min(minEndingHere*num, maxEndingHere*num, num)
-        maximum = max(maximum, maxEndingHere)
-    return maximum
+        minEndingHere = min(maxEndingHere*num, minEndingHere*num, num)
+        maxSoFar = max(maxSoFar, maxEndingHere)
+
+    return maxSoFar
 
 
-n = [random.randint(-10, 20) for _ in range(10)]
-print(n)
-print('Max:', maxContinuousProduct(n))
+if __name__ == '__main__':
+    input = [-6,-3,8,-9,-1,-1,3,6,9,0,3,-1]
+    print(maxProductSubArray(input))
